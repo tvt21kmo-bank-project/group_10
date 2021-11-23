0a4b939a61aa340a7e8c4e67dfac1482eb81d0ac
jaakko.mursu <jaakko.mursu@f-solutions.fi>
2021-11-23 16:56:00 +0200

    Formatted code to support hotcode reload

--- restapi/app.js
+++ restapi/app.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+module.exports = () => {
+    var express = require('express');
+    var path = require('path');
+    var cookieParser = require('cookie-parser');
+    var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+    var indexRouter = require('./routes/index');
+    var usersRouter = require('./routes/users');
+    var loginRouter = require('./routes/login');
 
-var app = express();
+    var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+    app.use('/', indexRouter);
+    app.use('/users', usersRouter);
+    app.use('/login', loginRouter);
 
-module.exports = app;
+    return app;
+}
