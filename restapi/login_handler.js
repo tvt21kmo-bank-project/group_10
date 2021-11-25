5cea80103df2a2d32c3c3e991e451f553b209845
jaakko.mursu <jaakko.mursu@f-solutions.fi>
2021-11-25 22:07:52 +0200

    decode Korttinumero from valid jwt for auth

--- restapi/login_handler.js
+++ restapi/login_handler.js
@@ -1,20 +1,19 @@
 var jwt = require('jsonwebtoken');
 
-var jwt_list = []
 module.exports = {
     inspect_jwt: function (given_token) {
-        if (jwt_list.includes(given_token)) {
-            try {
-                jwt.verify(given_token, process.env.JWT_SECRET)
-                console.log('User has logged in.')
-            } catch (error) {
-                throw 'You are not logged in.';
+        try {
+            if (jwt.verify(given_token, process.env.JWT_SECRET)) {
+                var user = jwt.decode(given_token, { complete: true });
+                console.log(user.payload.card_num + ' has successfully logged in.')
+                return user.payload.card_num;
             }
+        } catch (error) {
+            throw 'You are not logged in.';
         }
     },
     sign_jwt: function (user) {
-        signed_token = jwt.sign({ exp: Math.floor(Date.now() / 1000) + (60 * 60), issuer: 'FooBank' }, process.env.JWT_SECRET);
-        jwt_list.push(signed_token);
+        signed_token = jwt.sign({ exp: Math.floor(Date.now() / 1000) + (60 * 60), card_num: user, issuer: 'FooBank' }, process.env.JWT_SECRET);
         return signed_token;
     }
 }
