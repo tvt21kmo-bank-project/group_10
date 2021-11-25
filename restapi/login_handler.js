ea212b94a4c8100bce3d448ff8224cfabcedaadc
jaakko.mursu <jaakko.mursu@f-solutions.fi>
2021-11-25 16:14:32 +0200

    Edited login handler to sign and verify jwt

--- restapi/login_handler.js
+++ restapi/login_handler.js
@@ -1,19 +1,20 @@
-var { createHash } = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var jwt_list = []
 module.exports = {
-    add_jwt: function (jwt) {
-        jwt_list.push(jwt);
-    },
-    hash: function (string, salt) {
-        return createHash('sha256').update(string + salt).digest('hex');
-    },
-    inspect_jwt: function (jwt) {
-        if (jwt_list.includes(jwt)) {
-            console.log('User has logged in.')
+    inspect_jwt: function (given_token) {
+        if (jwt_list.includes(given_token)) {
+            try {
+                jwt.verify(given_token, process.env.JWT_SECRET)
+                console.log('User has logged in.')
+            } catch (error) {
+                throw 'You are not logged in.';
+            }
         }
-        else {
-            throw 'You are not logged in.';
-        }
+    },
+    sign_jwt: function (user) {
+        signed_token = jwt.sign({ exp: Math.floor(Date.now() / 1000) + (60 * 60), issuer: 'FooBank' }, process.env.JWT_SECRET);
+        jwt_list.push(signed_token);
+        return signed_token;
     }
 }
