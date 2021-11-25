ed6c4fbabf1482a3686082c5a1928b227dcb6d66
jaakko.mursu <jaakko.mursu@f-solutions.fi>
2021-11-25 16:17:33 +0200

    login handling for backend to initiate user sess

--- restapi/routes/login.js
+++ restapi/routes/login.js
@@ -1,17 +1,38 @@
 var express = require('express');
 var router = express.Router();
-var pool = require('../db_handler');
+var pool = require('../db_handler')();
+var { sign_jwt } = require('../login_handler');
 const { createHash } = require("crypto");
 
 router.post('/', function (req, res, next) {
     pool.getConnection(function (err, connection) {
-        var credentials = JSON.parse(req.header('data'));
-        var user = connection.query('Select * where AccountId =' + credentials.id);
-        if (user.passwd == hash(req.passwd, user.salt)) {
-            res.send('jwt provided login success');
-            connection.release();
-            return;
+        console.log(req.headers)
+        console.log(req.body)
+        if (!req.body.id || !req.body.passwd) {
+            res.status(401).send('id or passwd not provided.')
         }
+        connection.query('Select * from Kortti where Korttinumero =' + req.body.id, function (err, user, fields) {
+            if (user.length === 0) {
+                res.status(401).send('login väärin')
+                return;
+            }
+            else if (user[0].PIN == createHash('sha256').update(req.body.passwd + user[0].salt).digest('hex')) {
+                tokeni = sign_jwt(user[0].Korttinumero);
+                res.send({ 'token': tokeni });
+                connection.release();
+                return;
+            }
+            else if (err) {
+                res.status(500).send(err);
+                connection.release();
+                return;
+            }
+            else {
+                res.status(401).send('login väärin');
+                connection.release();
+                return;
+            }
+        });
     });
 });
 
